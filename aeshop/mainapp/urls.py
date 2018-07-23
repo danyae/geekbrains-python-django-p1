df79aa3d60bf2a73f08fd80000965380d55c2d7c
ae <danyae@users.noreply.github.com>
2018-07-23 23:19:43 +0300

    Changed views to class-based and added views to edit and delete products

--- aeshop/mainapp/urls.py
+++ aeshop/mainapp/urls.py
@@ -3,6 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    re_path(r'^post/(?P<pk>\d+)/$', views.product, name='product'),
-    path('product/add/', views.product_add, name='product_add'),
+    # re_path(r'^post/(?P<pk>\d+)/$', views.product, name='product'),
+    re_path(r'^product/(?P<pk>\d+)/$',
+            views.ProductDetail.as_view(), name='product_detail'),
+    # path('product/add/', views.product_add, name='product_add'),
+    path('product/create/', views.ProductCreate.as_view(), name='product_create'),
+    path('product/update/<int:pk>/',
+         views.ProductUpdate.as_view(), name='product_update'),
+    path('product/delete/<int:pk>/',
+         views.ProductDelete.as_view(), name='product_delete'),
 ]
--- aeshop/mainapp/views.py
+++ aeshop/mainapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Product, Producer
 from .forms import ProductForm
 # Create your views here.
@@ -12,15 +14,21 @@ def index(request):
             producer__name=producer.name)
         if len(products_for_producer) > 0:
             products_dict[producer] = products_for_producer
-    return render(request, 'mainapp/index.html', {'products_dict': products_dict})
+    return render(request, 'mainapp/index.html',
+                  {'products_dict': products_dict})
 
 
-def product(request, pk):
-    product_obj = get_object_or_404(Product, pk=pk)
-    producs_same_producer = Product.objects.filter(
-        producer=product_obj.producer).exclude(pk=product_obj.pk)
-    return render(request, 'mainapp/product.html', {'product': product_obj,
-                                                    'other_products': producs_same_producer})
+class ProductDetail(DetailView):
+    model = Product
+    context_object_name = 'product'
+    template_name = 'mainapp/product.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetail, self).get_context_data(**kwargs)
+        # Products of the same producer
+        context['other_products'] = Product.objects.filter(
+            producer=self.object.producer).exclude(pk=self.object.pk)
+        return context
 
 
 def product_add(request):
@@ -32,3 +40,29 @@ def product_add(request):
     else:
         form = ProductForm()
     return render(request, 'mainapp/product/add.html', {'form': form})
+
+
+class ProductCreate(CreateView):
+    model = Product
+    fields = ['name', 'price', 'text', 'producer', 'image']
+    template_name = 'mainapp/product/create.html'
+
+    def get_success_url(self):
+        success_url = reverse_lazy('product_detail', args=(self.object.pk,))
+        return success_url
+
+
+class ProductUpdate(UpdateView):
+    model = Product
+    fields = ['name', 'price', 'text', 'producer', 'image']
+    template_name = 'mainapp/product/create.html'
+
+    def get_success_url(self):
+        success_url = reverse_lazy('product_detail', args=(self.object.pk,))
+        return success_url
+
+
+class ProductDelete(DeleteView):
+    model = Product
+    template_name = 'mainapp/product/confirm_delete.html'
+    success_url = reverse_lazy('index')
